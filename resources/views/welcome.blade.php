9115c3b7920b9547b77b40f255d1226a15f076e0
Kirill Chernov <sireler@gmail.com>
2020-11-14 00:48:27 +0300

    main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,121 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="section">
+            <div class="container my-3">
+                <div class="row">
+                    <div class="col border-right">
+                        <img src="img/banner-main.png" alt="services" width="600px">
+                    </div>
+
+                    <div class="col text-center">
+                        <a class="btn-link-main" style="font-size: 32px" href="#">Отследить</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row border-top border-bottom">
+                <div class="container">
+                    <div class="row" style="padding: 16px;">
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Письма</a>
+                        </div>
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Отправить посылку</a>
+                        </div>
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Вызвать курьера</a>
+                        </div>
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Платежи и переводы</a>
+                        </div>
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Отделения</a>
+                        </div>
+                        <div class="col-auto">
+                            <a class="btn-link-main" style="font-size: 18px" href="#">Другие сервисы</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col-auto border footer-cards" style="width: 300px">
+                    <a href="#" style="text-decoration: none; color: gray; line-height: 1;">
+                        <row class="row">
+                            <div class="col-3">
+                                <img src="img/Эзп%20обновление%20June.png" alt="" style="width: 100%;">
+                            </div>
+                            <div class="col">
+                                <h3 class="btn-link-main" style="font-size: 18px;">Заказные письма</h3>
+                                <p style="font-size: 14px;">Бесконтактная онлайн отправка и получение</p>
+                            </div>
+                        </row>
+                    </a>
+                </div>
+                <div class="col-auto border footer-cards" style="width: 300px">
+                    <a href="#" style="text-decoration: none; color: gray; line-height: 1;">
+                        <row class="row">
+                            <div class="col-3">
+                                <img src="img/Эзп%20обновление%20June.png" alt="" style="width: 100%;">
+                            </div>
+                            <div class="col">
+                                <h3 class="btn-link-main" style="font-size: 18px;">Заказные письма</h3>
+                                <p style="font-size: 14px;">Бесконтактная онлайн отправка и получение</p>
+                            </div>
+                        </row>
+                    </a>
+                </div>
+                <div class="col-auto border footer-cards" style="width: 300px">
+                    <a href="#" style="text-decoration: none; color: gray; line-height: 1;">
+                        <row class="row">
+                            <div class="col-3">
+                                <img src="img/Эзп%20обновление%20June.png" alt="" style="width: 100%;">
+                            </div>
+                            <div class="col">
+                                <h3 class="btn-link-main" style="font-size: 18px;">Заказные письма</h3>
+                                <p style="font-size: 14px;">Бесконтактная онлайн отправка и получение</p>
+                            </div>
+                        </row>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-5">
+                    <span>2020 © АО Почта России</span>
+                </div>
+                <div class="col">
+                    <div class="row">
+                        <div class="col-auto">
+                            <i class="fab fa-vk"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-instagram"></i>
+                            <i class="fab fa-odnoklassniki"></i>
+                            <i class="fab fa-twitter"></i>
+                        </div>
+                        <div class="col-auto">
+                            <a href="#">Раскрытие информации</a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="#">Вакансии</a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="#">Пресс-центр</a>
+                        </div>
+                        <div class="col-auto">
+                            <a href="#">О компании</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+
     </div>
 @endsection
