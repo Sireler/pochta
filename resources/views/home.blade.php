a09fe7e5aa8ca4c96dee05bb602d2b7e12d6898a
Rebroshevich WhatIsLove <turgenerator@gmail.com>
2020-11-14 23:47:31 +0300

    end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,36 +22,35 @@
                             <div class="col-md-9">
                                 <h2 class="text-secondary">Здравствуйте, {{ Auth::user()->name }}</h2>
                                 <div class="text-primary font-weight-bold"><h4 class="ml-4">Общая статистика:</h4></div>
-                                <div class="row justify-content-center">
+                                <div class="row ">
 
-                                    <div class="col-md-6">
+                                    <div class="col-6">
                                         <div class="text-center alert alert-primary font-weight-bold">
-                                            <h3>Загружено файлов:</h3>
+                                            <h3>Всего загружено фалов:</h3>
                                             <div class="display-4">
                                                 {{ $filesUploaded ?? 0 }}
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-6">
                                         <div class="text-center alert alert-primary font-weight-bold">
-                                            <h3>Обработано строк:</h3>
+                                            <h3>Всего обработано строк:</h3>
                                             <div class="display-4">
                                                 {{ $rowsProcessed ?? 0 }}
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="offset-md-4"></div>
-                                    <div class="col-md-4">
+                                    <div class="col-6">
                                         <div class="text-center alert alert-success font-weight-bold">
-                                            <h3>Успешно:</h3>
+                                            <h3>Всего успешно обработанных строк:</h3>
                                             <div class="display-4">
                                                 {{ $successRowsProcessed ?? 0 }}
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-6">
                                         <div class="text-center alert alert-warning font-weight-bold">
-                                            <h3>Ошибка:</h3>
+                                            <h3>Всего исправлено файлов:</h3>
                                             <div class="display-4">
                                                 {{ $warningRowsProcessed ?? 0 }}
                                             </div>
